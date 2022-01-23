3cc8c8f068471fe733368350763511e7b31a6996
malatini <malatini@student.42.fr>
2022-01-23 20:54:41 +0100

    reprise avec typescript

---
+++ front/typescript-react-app/src/Navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import './Navbar.css';
+
+export default function Navbar() {
+    return (
+        <div>
+            {/* <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">ft_transcendance</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link active" href="#">Accueil<span class="sr-only"></span></a>
+                        <a class="nav-item nav-link disabled" href="#">Disabled</a>
+                    </div>
+                </div>
+            </nav> */}
+        </div>
+    );
+}
---
+++ front/typescript-react-app/src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <head>
+    </head>
+    <body>
+      <App />
+    </body>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
