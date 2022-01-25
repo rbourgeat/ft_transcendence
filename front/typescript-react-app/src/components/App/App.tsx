47836c056113a3bd2f8b763d9da9ba2a4d224d28
malatini42 <malatini@student.42.fr>
2022-01-25 16:08:17 +0100

    pb div profile

--- front/typescript-react-app/src/components/App/App.tsx
+++ front/typescript-react-app/src/components/App/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import Sidebar from '../Sidebar/Sidebar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '../Pages/Home'
+import Profile from '../Profile/Profile'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <>
+    <div className="app-div">
       <Router>
         <Sidebar />
+        <Profile />
       </Router>
-    </>
+    </div>
   );
 }
 
