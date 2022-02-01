1f46c819a0694f24eb8f1b431d1721297aec567b
krolhm <raphaelb1@icloud.com>
2022-02-01 19:24:14 +0100

    connection 42 api done but 404 when request /v2/me

--- backend/src/db/dbinit.sql
+++ backend/src/db/dbinit.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users
 (
     id integer NOT NULL,
-    user text  NOT NULL,
+    login42 text  NOT NULL,
     avatar text  NOT NULL,
     status integer NOT NULL,
     wins integer DEFAULT 0,
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,25 @@ const app = express();
 
 const PORT = process.env.PORT || '80';
 
-function login() {
-	var access_token = "";
+function me(token) {
+	var options = {
+		'method': 'POST',
+		'url': 'https://api.intra.42.fr/v2/me',
+		'headers': {
+			'Content-Type': 'application/x-www-form-urlencoded'
+		},
+		form: {
+			'access_token': token
+		},
+		json: true
+	};
+	request(options, function (error, response) {
+		if (error) throw new Error(error);
+			console.log(response.body);
+	});
+}
+
+function auth(code, state) {
 	var options = {
 		'method': 'POST',
 		'url': 'https://api.intra.42.fr/oauth/token',
@@ -13,20 +30,21 @@ function login() {
 			'Content-Type': 'application/x-www-form-urlencoded'
 		},
 		form: {
-			'grant_type': 'client_credentials',
+			'grant_type': 'authorization_code',
 			'client_id': 'f60dc2f1d07980f192aaa1f0205dad411ee73dd20086150ffb9f62d7f7e0c901',
-			'client_secret': 'ea3c557c987c33a5f364461fa7ed4fae295142efff2906168d71c05b99f47ccd'
+			'client_secret': 'ea3c557c987c33a5f364461fa7ed4fae295142efff2906168d71c05b99f47ccd',
+			'code': code,
+			'state': state,
+			'redirect_uri': 'http://localhost/auth',
 		},
 		json: true
 	};
 	request(options, function (error, response) {
 		if (error) throw new Error(error);
 			console.log(response.body);
-		const obj = JSON.parse(response.body);
-		access_token = obj.access_token;
-		console.log("acces_token = " + access_token);
+		console.log('access_token = ' + response.body['access_token']);
+		me(response.body['access_token']);
 	});
-	//console.log("acces_token = " + access_token);
 }
 
 function makeid(length) {
@@ -51,13 +69,7 @@ app.get("/game", (req, res) => {
 	res.render("game", { user: "rbourgea"});
 });
 
-app.get("/error", (req, res) => {
-	res.render("error");
-});
-
 app.get("/login", (req, res) => {
-	//res.render("login");
-	//login();
 	var randomverylongstring = makeid(15);
 	res.writeHead(301, {
 		Location: `https://api.intra.42.fr/oauth/authorize?client_id=f60dc2f1d07980f192aaa1f0205dad411ee73dd20086150ffb9f62d7f7e0c901&redirect_uri=http%3A%2F%2Flocalhost%2Fauth&response_type=code&state=${randomverylongstring}`
@@ -65,7 +77,19 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/auth", (req, res) => {
-	res.render("auth");
+	var url = req.url;
+	var code = url.substring(url.indexOf("=") + 1, url.lastIndexOf("&"));
+	var state = url.substring(url.indexOf("state=") + 6, url.length - 1);
+	res.send("CODE = " + code + "\nSTATE = " + state);
+	if (code && state)
+		auth(code, state);
+	else
+		res.send("ERROR WITH 42 API");
+});
+
+app.get("/callback", (req, res) => {
+	res.send("YESSSS !");
+	
 });
 
 app.listen(PORT, () => {
