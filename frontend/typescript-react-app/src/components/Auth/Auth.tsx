ac40892f3b2adb03a4b2a0351b0107a9e0c38adb
malatini42 <malatini@student.42.fr>
2022-02-14 15:44:25 +0100

    reprise front static en cours

--- frontend/typescript-react-app/src/components/Auth/Auth.tsx
+++ frontend/typescript-react-app/src/components/Auth/Auth.tsx
@@ -13,23 +13,59 @@ export default function Auth() {
         < div id="auth-form-div" >
 
             <div id="auth--form" className="d-flex justify-content-center">
-                <Form>
-                    <h2>Connectez-vous !</h2>
+                <Form id="form-auth" className="forms">
+                    <h2>Se connecter</h2>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Student login </Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            Entrez votre login et non votre adresse mail.
-                        </Form.Text>
+                        <Form.Label>Login</Form.Label>
+                        <Form.Control type="email" placeholder="Login" />
+                        {/* <Form.Text className="text-muted">
+                            Entrez votre login
+                        </Form.Text> */}
                     </Form.Group>
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Mot de passe</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Mot de passe" />
                     </Form.Group>
-                    <Button variant="primary" type="submit" onClick={routeChange}>
-                        Me connecter
-                    </Button>
+                    {/* <Button className="btn btn-primary btn-lg btn-block" type="submit" onClick={routeChange}>
+                        Je me connecte
+                    </Button> */}
+                    <p></p>
+                    {/* <Button className="btn btn-primary btn-lg btn-block" type="submit" onClick={routeChange}>
+                        Authentification API 42
+                    </Button> */}
+                    <Form.Group className="mb-3" controlId="formBasicConnexion">
+                        <button type="submit" className="btn btn-secondary btn-block" onClick={routeChange} id="auth-btn-1">Block level button</button>
+                    </Form.Group>
+                    
+                </Form>
+                
+                <Form className="forms">
+                    <h2>S'inscrire</h2>
+                    <Form.Group className="mb-3" controlId="formSignupUsername">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="username" placeholder="malatini42" />
+                        {/* <Form.Text className="text-muted">
+                            Entrez votre login et non votre adresse mail.
+                        </Form.Text> */}
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formSignUpEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="malatini@student.42.fr" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formSignupPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="******" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formSignupConfirmPassword">
+                        <Form.Label>Confirm Email</Form.Label>
+                        <Form.Control type="confirmemail" placeholder="******" />
+                    </Form.Group>
+
+                    <button type="submit" className="btn btn-secondary btn-block" onClick={routeChange} id="auth-btn-2">M'inscrire</button>
                 </Form>
             </div>
         </div >);
