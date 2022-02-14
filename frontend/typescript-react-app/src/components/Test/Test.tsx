691d82ad7850cc27262a3a2332922a7ebf7d3c61
malatini42 <malatini@student.42.fr>
2022-02-14 17:21:22 +0100

    tuto wavy curves en cours Mahaut

---
+++ frontend/typescript-react-app/src/components/Test/Test.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './Test.css';
+
+function Test() {
+    return (
+        <div id="test">
+        <section>
+            <h1>Nice Curves</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus eum neque perspiciatis, eius ut temporibus? Laborum, optio eligendi perferendis ipsa iusto totam nostrum doloribus magni, aspernatur corporis tempora labore qui.</p>
+        </section>
+        <section>
+            <h1>Nice Curves</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus eum neque perspiciatis, eius ut temporibus? Laborum, optio eligendi perferendis ipsa iusto totam nostrum doloribus magni, aspernatur corporis tempora labore qui.</p>
+        </section>
+        <section>
+            <h1>Nice Curves</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus eum neque perspiciatis, eius ut temporibus? Laborum, optio eligendi perferendis ipsa iusto totam nostrum doloribus magni, aspernatur corporis tempora labore qui.</p>
+        </section>
+        <section>
+            <h1>Nice Curves</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus eum neque perspiciatis, eius ut temporibus? Laborum, optio eligendi perferendis ipsa iusto totam nostrum doloribus magni, aspernatur corporis tempora labore qui.</p>
+        </section>
+        <section>
+            <h1>Nice Curves</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus eum neque perspiciatis, eius ut temporibus? Laborum, optio eligendi perferendis ipsa iusto totam nostrum doloribus magni, aspernatur corporis tempora labore qui.</p>
+        </section>
+        <section>
+            <h1>Nice Curves</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus eum neque perspiciatis, eius ut temporibus? Laborum, optio eligendi perferendis ipsa iusto totam nostrum doloribus magni, aspernatur corporis tempora labore qui.</p>
+        </section>
+        <section>
+            <h1>Nice Curves</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus eum neque perspiciatis, eius ut temporibus? Laborum, optio eligendi perferendis ipsa iusto totam nostrum doloribus magni, aspernatur corporis tempora labore qui.</p>
+        </section>
+        <section>
+            <h1>Nice Curves</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus eum neque perspiciatis, eius ut temporibus? Laborum, optio eligendi perferendis ipsa iusto totam nostrum doloribus magni, aspernatur corporis tempora labore qui.</p>
+        </section>
+        </div>
+    );
+} export default Test;
