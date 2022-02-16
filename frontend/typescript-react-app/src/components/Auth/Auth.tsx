414430788043e5f1a1a8e1fde2e2e66778a168d9
malatini42 <mahautlatinisheritier@gmail.com>
2022-02-16 11:23:58 +0100

    un bouton affiche mot de passe, routing a revoir

--- frontend/typescript-react-app/src/components/Auth/Auth.tsx
+++ frontend/typescript-react-app/src/components/Auth/Auth.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, {useState }from 'react'
 import ReactDOM from 'react-dom';
 import './Auth.css';
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { Form, Button} from 'react-bootstrap';
+// import { trustedTypes } from 'trusted-types';
 export default function Auth() {
-    let navigate = useNavigate(); 
-  const routeChange = () =>{ 
-    let path = `home`; 
-    navigate(path);
-  }
+//     let navigate = useNavigate(); 
+//   const routeChange = () =>{ 
+//     let path = `home`; 
+//     navigate(path);
+//   };
+  
+    const [showPass, setshowPass] = useState(false);
+    console.log(showPass);
     return (
         <div>
         <div className="wrapper">
@@ -18,7 +22,9 @@ export default function Auth() {
            
             <div id="auth--form" className="d-flex justify-content-center">
                 <Form id="form-auth" className="forms">
-                    <h2>Se connecter</h2>
+                    <h2>Bienvenue ! </h2>
+                    <p>Pour jouer, vous devez vous authentifier 🤩</p>
+                    <h3>Se connecter</h3>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Login</Form.Label>
                         <Form.Control type="email" placeholder="Login" />
@@ -29,17 +35,20 @@ export default function Auth() {
                         <Form.Control type="password" placeholder="Mot de passe" />
                     </Form.Group>
                     <p></p>
+                    {/* onClick={routeChange}  */}
                     <Form.Group className="mb-3" controlId="formBasicConnexion">
-                        <button type="submit" className="btn btn-light btn-block" onClick={routeChange} id="auth-btn-1">Me connecter</button>
+                        <button type="submit" className="btn btn-light btn-block" id="auth-btn-1" >Me connecter</button>
                     </Form.Group>
-                    <hr className="my-4"></hr>
+                    {/* <hr className="my-4"></hr> */}
+                    {/* onClick={routeChange} */}
                     <Form.Group className="mb-3" controlId="formAuth42">
-                        <button type="submit" className="btn btn-light btn-block" onClick={routeChange} id="auth-btn-3">Authentification 42</button>
+                        <button type="submit" className="btn btn-primary btn-block" id="auth-btn-3">Authentification 42</button>
+
                     </Form.Group>
                 </Form>
                 
                 <Form className="forms">
-                    <h2>S'inscrire</h2>
+                    <h3>S'inscrire</h3>
                     <Form.Group className="mb-3" controlId="formSignupUsername">
                         <Form.Label>Username</Form.Label>
                         <Form.Control type="username" placeholder="malatini42" />
@@ -49,16 +58,38 @@ export default function Auth() {
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="email" placeholder="malatini@student.42.fr" />
                     </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formSignupPassword">
+                
+                    {/* <div className="mb-3" id="formSignupPassword"> */}
+                    <div id="password-form">
                         {/* Pour ajouter des regles sur le password : https://developer.mozilla.org/fr/docs/Web/HTML/Element/Input/password */}
-                        <Form.Label>Password</Form.Label>
-                        <i className="far fa-eye" id="togglePassword"></i>
-                        <Form.Control type="password" placeholder="******" />
-                        {/* Ajouter un post ici ? */}
-                    </Form.Group>
-                    <button type="submit" className="btn btn-light btn-block" onClick={routeChange} id="auth-btn-2">M'inscrire</button>
-                </Form>
+                        <div id="sub-form">
+                            <div>
+                            {/* className="col-lg-6" */}
+                                <form>
+                                    <div>
+                                        <label>
+                                        Password
+                                        </label>
+                                        <input className="form-control" type={showPass? "text":"password"} />
+                                    </div>
+                                        <div className="mt-2" style={{textAlign:"right"}} id="show-password">
+                                            <button type="button" className="btn btn-sm btn-light" onClick={() => {
+                                                if (showPass == true)
+                                                    setshowPass(false);
+                                                else 
+                                                    setshowPass(true);
+                                            }}>
+                                                {showPass ? "Cacher" : "Afficher"}
+                                            </button>
+                                        </div>
+                                    </form>
+                            </div>
+                        </div>
+                    </div>
+                    <p></p>
+                    <hr className="my-4"></hr>
+                    <button type="submit" className="btn btn-light btn-block" id="signup">M'inscrire</button>
+                    </Form>
             </div>
         </div >
         </div>
