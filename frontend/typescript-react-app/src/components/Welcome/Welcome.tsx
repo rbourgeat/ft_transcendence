babf9e24246778c156ca18ebc898d3de000c058d
malatini42 <malatini@student.42.fr>
2022-02-19 12:00:58 +0100

    creation welcome page front en cours

--- frontend/typescript-react-app/src/components/Welcome/Welcome.tsx
+++ frontend/typescript-react-app/src/components/Welcome/Welcome.tsx
@@ -3,17 +3,24 @@ import './Welcome.scss';
 import '../App/App.scss';
 import '../../index.scss';
 
-import Footer from "../Footer/Footer"
-import Header from "../Header/Header"
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+
+import video1 from "../../images/pong-demo.mp4";
 
 // import { useNavigate } from "react-router-dom";
 //TODO: a ranger dans Pages
 export default function Welcome() {
     return (
-        <div id="welcome">
-            Welcome page
+        <>
+        <Header />
+        <div id="welcome--video-div">
+            This is our inspiration
+            <div>
+                <video src={video1} autoPlay loop id="welcome--video-video"/>
+            </div>
             <Footer />
         </div>
-        
+        </>
     )
 }
--- frontend/typescript-react-app/src/react-app-env.d.ts
+++ frontend/typescript-react-app/src/react-app-env.d.ts
@@ -1,1 +1,4 @@
-
+declare module '*.mp4' {
+    const src: string;
+    export default src;
+  }
