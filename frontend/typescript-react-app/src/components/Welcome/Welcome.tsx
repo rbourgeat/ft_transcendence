6326653d57d0c0b04da768665575a227c4c2c24d
malatini42 <malatini@student.42.fr>
2022-02-20 11:26:40 +0100

    animation type writer authors

--- frontend/typescript-react-app/src/components/Welcome/Welcome.tsx
+++ frontend/typescript-react-app/src/components/Welcome/Welcome.tsx
@@ -8,6 +8,8 @@ import Header from "../Header/Header";
 
 import video1 from "../../images/pong-demo.mp4";
 
+import TypeAnimation from 'react-type-animation';
+
 //TODO: a ranger dans Pages
 
 export default function Welcome() {
@@ -16,12 +18,28 @@ export default function Welcome() {
         <Header />
         <div id="welcome--video-div-main">
             {/* This is our inspiration */}
-            <p></p>
+            <div id="authors">
+                <ul id="authors--ul">
+                <TypeAnimation
+                    cursor={true}
+                    sequence={['Authors :', 5000, '']}
+                    wrapper="figcaption"
+                    repeat="Infitiny"
+                />
+                {/* <figcaption>Authors : </figcaption> */}
+                <br></br>
+                    <li>bahaas</li>
+                    <li>rbourgea</li>
+                    <li>malatini</li>
+                </ul>
+            </div>
+            <br></br>
             <div  id="welcome--video-div">
                 <div id="test">
                     <video autoPlay loop muted src={video1}  height="200" id="welcome--video-video"/>
                 </div>
             </div>
+            
             <Footer />
         </div>
         </>
