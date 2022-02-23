43bbae2d48fe2ab6addfead326f918c75a8a6b8a
bahaas <baudoin.haas@gmail.com>
2022-02-23 18:31:24 +0100

    refacto back structure

--- backend-nestjs/ormconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    'type': 'postgres',
-    'url': process.env.DB_URL,
-    'entities': [process.env.ENTITY_PATH],
-    'synchronize': true
-  }
--- backend-nestjs/src/user/dto/user.dto.ts
+++
@@ -1,9 +0,0 @@
-import {IsNumber, IsString} from "class-validator";
-
-export class UserDto {
-    @IsString()
-    name: string;
-
-    @IsString()
-    avatar: string;
-}
