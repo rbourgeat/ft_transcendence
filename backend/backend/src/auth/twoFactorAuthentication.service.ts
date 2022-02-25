1de8ffee7b735d1643bfa906bfc0e07573ca4b0e
bahaas <baudoin.haas@gmail.com>
2022-02-25 15:54:49 +0100

    fix port to have front & back

--- backend/backend/src/auth/twoFactorAuthentication.service.ts
+++ backend/backend/src/auth/twoFactorAuthentication.service.ts
@@ -2,21 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { authenticator } from 'otplib';
 import { User } from '../user/user.entity';
 import { UserService } from '../user/user.service';
- 
+import { ConfigService } from '@nestjs/config';
+
 @Injectable()
 export class TwoFactorAuthenticationService {
-  constructor (
+  constructor(
     private readonly userService: UserService,
     private readonly configService: ConfigService
-  ) {}
+  ) { }
 
   public async generateTwoFactorAuthenticationSecret(user: User) {
     const secret = authenticator.generateSecret();
- 
+
     const otpauthUrl = authenticator.keyuri(user.email, this.configService.get('TWO_FACTOR_AUTHENTICATION_APP_NAME'), secret);
- 
-    await this.userService.setTwoFactorAuthenticationSecret(user.login, secret);
- 
+
+    await this.userService.setupTwoFactorAuthentication(user.login, secret);
+
     return {
       secret,
       otpauthUrl
