61b23d1d0502d7a704279edf0ace300b78ce6acf
malatini42 <malatini@student.42.fr>
2022-03-02 15:29:58 +0100

    ok docker

--- frontend/typescript-react-app/src/components/Test/Test.tsx
+++ frontend/typescript-react-app/src/components/Test/Test.tsx
@@ -1,19 +1,10 @@
 import React, { useState } from 'react';
 import './Test.scss';
-import Wave from 'react-wavify'
+//import Wave from 'react-wavify'
 
 function Test() {
     return (
         <div>
-              <Wave fill='#f79902'
-                    paused={false}
-                    options={{
-                    height: 20,
-                    amplitude: 20,
-                    speed: 0.15,
-                    points: 3
-                    }}
-            />
         </div>
 
     );
