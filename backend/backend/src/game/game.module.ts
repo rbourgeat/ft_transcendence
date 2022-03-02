585d61a540861d945862cfa11037ec9c49046134
Raphael bourgeat <cosmosraph@gmail.com>
2022-03-02 13:41:51 +0100

    test perm

--- backend/backend/src/game/game.module.ts
+++ backend/backend/src/game/game.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
 import { Game } from './game.entity'
-import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Game])],
