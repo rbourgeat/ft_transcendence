4e2f29d0f901199d2eaaa1cbae4666292dc8ab31
malatini42 <malatini@student.42.fr>
2022-03-05 19:37:46 +0100

    state.avatar plutot que get avatar

--- frontend/typescript-react-app/src/components/Axios/Axios.tsx
+++ frontend/typescript-react-app/src/components/Axios/Axios.tsx
@@ -14,8 +14,35 @@ interface AxiosProps {
 
 interface AxiosState {
     status?: number,
-    rep?: Object
+    rep?: Object,
+    avatar?: string
 }
+
+//interface User {
+//    data: {
+//        avatar?: string,
+//        status?: string,
+//        total_games?: boolean,
+//        total_wins?: boolean,
+//        total_loss?: boolean,
+//        win_loss_ration?: boolean,
+//        two_factor_auth?: boolean,
+//        id?: number,
+//        createdAt?: Date,
+//        updateAt?: Date,
+//        login?: string,
+//        email?: string,
+//        password?: string,
+//        //friends: Array,
+//        //"two_factor_secret": "false",
+//        //"is_ban": false,
+//        //"is_admin": false,
+//        //"blocked_users": null,
+//        //"achievements": [],
+//        login42: string
+//    }
+//}
+
 
 export default class MyAxios extends React.Component<AxiosProps, AxiosState>
 {
@@ -104,24 +131,68 @@ export default class MyAxios extends React.Component<AxiosProps, AxiosState>
             let res = axios.post('http://localhost:3000/api/auth/register/', bod, {headers}).then(res=>{
                 console.log(res.data);
                 console.log(res.status)
-
                 if (res.status == 201)
                 {
-                toast.notifySuccess('🦄 Yes! You are now registered ! You may log in.');
+                    if (res.data.avatar)
+                    {
+                        toast.notifySuccess('🦄 Yes! You are now registered ! You may log in.');
+                        return (res.data);
+                    }
+
                 }
                 else
                 {
-                toast.notifyDanger('Oops ! An error happened');
+                    toast.notifyDanger('Oops ! An error happened');
+                    return (null);
                 }
             }).catch((error) => {
+                console.log("Catched error !");
                 console.log(error);
-                toast.notifyDanger('Oops ! An error happened');
+                //toast.notifyDanger('Oops ! An error happened');
+                return (null);
             })
     }
 
     //a reprendre quand ok pour user
-    getavatar()
+    get_avatar(url: string): string
     {
+        axios.defaults.baseURL = 'http://localhost:3000/';
 
+        let toast = new ToastAlerts(null);
+
+        let res = axios.get(url)
+            .then(res=>
+            {
+                //console.log(res.data.status);
+                console.log(res.statusText);
+                //console.log(res.data.avatar);
+                //console.log(res.status)
+                if (res.statusText == "OK")
+                {
+                    //toast.notifySuccess('Avatar retrieved successfully');
+                    //toast.notifySuccess(res.data.avatar);
+                    //let avatar= new string(res.data.avatar);
+                    const { data } = res;
+                    let test: any = JSON.parse(res.data.avatar);
+                    let avatar: string = ((data.avatar) as string);
+
+                    //let avatar: string = data.avatar as string;
+                    //console.log(typeof avatar)
+                    return (this.state.avatar);
+                }
+                else
+                {
+                    toast.notifyDanger('🥲 Avatar was not retrieved successfully');
+                    return ("https://pbs.twimg.com/profile_images/1380427848075317248/nxgi57Th_400x400.jpg" as string);
+                }
+            }
+            )
+            .catch((error) => {
+                console.log("Catched error !");
+                //console.log(error);
+                //toast.notifyDanger('Oops ! An error happened');
+                return ("https://pbs.twimg.com/profile_images/1380427848075317248/nxgi57Th_400x400.jpg");
+            })
+            return ("https://pbs.twimg.com/profile_images/1380427848075317248/nxgi57Th_400x400.jpg" as string);
     }
 }
--- frontend/typescript-react-app/src/components/User/User.tsx
+++ frontend/typescript-react-app/src/components/User/User.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect} from 'react';
 import Nav from "../Nav/Nav";
 import './User.scss';
 import axios from 'axios';
+import MyAxios from '../Axios/Axios';
+import { ToastContainer, toast } from 'react-toastify';
 
 //import { Link } from 'react-router-dom';
 //creer un user avec le contexte de la connection?
@@ -52,7 +54,8 @@ export default class User extends React.Component<UserProps, MyState>
       }
     }
 
-    get_avatar=(event) =>
+    //On va modifier le state
+    post_avatar=(event) =>
     {
       //if (this.props.username && this.props.username != "null")
       //{
@@ -60,13 +63,36 @@ export default class User extends React.Component<UserProps, MyState>
         axios.defaults.baseURL = 'http://localhost:3000/';
         axios.defaults.headers.get['Content-Type'] ='*';
         axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
-       let url = "http://localhost:3000/api/user/".concat(username);
+        let url = "http://localhost:3000/api/user/".concat(username);
         //+ this.props.username;
         console.log(url);
-        //let res = axios.get(url);
-        return (1);
+        let ax = new MyAxios({});
+        let avatar: string;
+        avatar = ax.get_avatar(url);
+        //return (avatar);
+        //console.log(res);
       //}
     }
+
+    //get_avatar(): string
+    //{
+    //  //if (this.props.username && this.props.username != "null")
+    //  //{
+    //    console.log("get avatar called");
+    //    let username = this.state.username;
+    //    axios.defaults.baseURL = 'http://localhost:3000/';
+    //    axios.defaults.headers.get['Content-Type'] ='*';
+    //    axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+    //   let url = "http://localhost:3000/api/user/".concat(username);
+    //    //+ this.props.username;
+    //    console.log(url);
+    //    let ax = new MyAxios({});
+    //    let avatar: string = ax.get_avatar(url)!;
+    //    console.log(avatar);
+    //    return (avatar);
+    //    //console.log(res);
+    //  //}
+    //}
 
   //il va falloir verifier que la valeur des props sont a jours
   render() {
@@ -76,22 +102,36 @@ export default class User extends React.Component<UserProps, MyState>
         <div className="container">
           <div className="row d-flex justify-content-center text-center">
             <div className="col-4" id="bonjour--user">
-              <h1>Bonjour
-                <span>{this.props.username ? this.props.username : ""} </span> !
+              <h1>Bonjour <span>{this.props.username ? this.props.username : ""} </span>
               </h1>
               <br />
               <div className="user--stats">
               <h2>Information</h2>
               <img
                 // src={this.props.avatar}
-                src="https://pbs.twimg.com/profile_images/1380427848075317248/nxgi57Th_400x400.jpg"
+                src={this.state.avatar}
                 height="80"
                 alt="avatar"
                 id="avatar-id"
                 />
                 <br/>
                 <br/>
-                <button onClick={this.get_avatar}>Change avatar</button>
+                <button
+                  /*onClick={this.get_avatar}*/
+                >
+                  Change avatar
+                </button>
+                <ToastContainer
+                                    position="top-right"
+                                    autoClose={5000}
+                                    hideProgressBar={false}
+                                    newestOnTop={false}
+                                    closeOnClick
+                                    rtl={false}
+                                    pauseOnFocusLoss
+                                    draggable
+                                    pauseOnHover
+                                />
                 <br />
                 <br/>
                 <p>Total games : <span className="span--stats">{this.state.totalGames ? this.state.totalGames : 0}</span></p>
