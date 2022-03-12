bed19d83279d6ed79fc8f727d7a631817b720d0a
malatini42 <mahautlatinisheritier@gmail.com>
2022-03-12 12:28:40 +0100

    achievements statique en cours

--- frontend/typescript-react-app/src/components/Achievements/Achievements.tsx
+++ frontend/typescript-react-app/src/components/Achievements/Achievements.tsx
@@ -30,13 +30,65 @@ export default class Achievements extends React.Component<AchievementsProps, Ach
 				<Nav />
 					<div id="achievements">
 						<div className="container">
-							<div className="row d-flex justify-content-center text-center">
-								<div className="col-7">
-									<h1>Achievements</h1>
-								</div>
-							</div>
-						</div>
-					</div>
+							<div className="row d-flex justify-content-center text-center" id="row-cards">
+                                <h1>Achievements</h1>
+                            </div>
+                                        <div className="row" id="first-row">
+                                        <div className="card-group">
+                                            <div className="card">
+                                                <div className="card-body">
+                                                    <h5 className="card-title">The challender</h5>
+                                                    <p className="card-text">Reach the first position in the ladder</p>
+                                                </div>
+                                            </div>
+
+                                            {/* Deuxieme */}
+                                            <div className="card">
+                                                <div className="card-body">
+                                                    <h5 className="card-title">The challender</h5>
+                                                    <p className="card-text">Reach the first position in the ladder</p>
+                                                </div>
+                                            </div>
+
+                                            {/* Troisieme */}
+                                            <div className="card">
+                                                <div className="card-body">
+                                                    <h5 className="card-title">The challender</h5>
+                                                    <p className="card-text">Reach the first position in the ladder</p>
+                                                </div>
+                                            </div>
+                                            
+
+                                        <div className="row" id="second-row">
+                                            <div className="card-group"></div>
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">The challender</h5>
+                                                        <p className="card-text">Reach the first position in the ladder</p>
+                                                    </div>
+                                                </div>
+
+                                                <div className="card-group"></div>
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">The challender</h5>
+                                                        <p className="card-text">Reach the first position in the ladder</p>
+                                                    </div>
+                                                </div>
+
+                                                <div className="card-group"></div>
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">The challender</h5>
+                                                        <p className="card-text">Reach the first position in the ladder</p>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
             	<Footer />
             </div>
         );
