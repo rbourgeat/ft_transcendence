65e9eaf165182e830d35eefe495bc941c5475afb
malatini42 <mahautlatinisheritier@gmail.com>
2022-03-12 12:41:40 +0100

    save achievements en cours

--- frontend/typescript-react-app/src/components/Achievements/Achievements.tsx
+++ frontend/typescript-react-app/src/components/Achievements/Achievements.tsx
@@ -34,33 +34,15 @@ export default class Achievements extends React.Component<AchievementsProps, Ach
                                 <h1>Achievements</h1>
                             </div>
                                         <div className="row" id="first-row">
-                                        <div className="card-group">
-                                            <div className="card">
-                                                <div className="card-body">
-                                                    <h5 className="card-title">The challender</h5>
-                                                    <p className="card-text">Reach the first position in the ladder</p>
-                                                </div>
-                                            </div>
+                                            <div className="card-group">
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">The challender</h5>
+                                                        <p className="card-text">Reach the first position in the ladder</p>
+                                                    </div>
+                                                 </div>
 
                                             {/* Deuxieme */}
-                                            <div className="card">
-                                                <div className="card-body">
-                                                    <h5 className="card-title">The challender</h5>
-                                                    <p className="card-text">Reach the first position in the ladder</p>
-                                                </div>
-                                            </div>
-
-                                            {/* Troisieme */}
-                                            <div className="card">
-                                                <div className="card-body">
-                                                    <h5 className="card-title">The challender</h5>
-                                                    <p className="card-text">Reach the first position in the ladder</p>
-                                                </div>
-                                            </div>
-                                            
-
-                                        <div className="row" id="second-row">
-                                            <div className="card-group"></div>
                                                 <div className="card">
                                                     <div className="card-body">
                                                         <h5 className="card-title">The challender</h5>
@@ -68,26 +50,70 @@ export default class Achievements extends React.Component<AchievementsProps, Ach
                                                     </div>
                                                 </div>
 
-                                                <div className="card-group"></div>
+                                                {/* Troisieme */}
                                                 <div className="card">
                                                     <div className="card-body">
                                                         <h5 className="card-title">The challender</h5>
                                                         <p className="card-text">Reach the first position in the ladder</p>
                                                     </div>
                                                 </div>
-
-                                                <div className="card-group"></div>
-                                                <div className="card">
-                                                    <div className="card-body">
-                                                        <h5 className="card-title">The challender</h5>
-                                                        <p className="card-text">Reach the first position in the ladder</p>
-                                                    </div>
-                                                </div>
-
                                             </div>
                                         </div>
+                                            
+                                        <div className="row" id="second-row">
+                                            <div className="card-group">
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">Bibi</h5>
+                                                        <p className="card-text">Reach the first position in the ladder</p>
+                                                    </div>
+                                                </div>
+
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">Baba</h5>
+                                                        <p className="card-text">Reach the first position in the ladder</p>
+                                                    </div>
+                                                </div>
+                                                
+
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">Bubu</h5>
+                                                        <p className="card-text">Reach the first position in the ladder</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="row" id="third-row">
+                                            <div className="card-group">
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">Bibi</h5>
+                                                        <p className="card-text">Reach the first position in the ladder</p>
+                                                    </div>
+                                                </div>
+
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">Baba</h5>
+                                                        <p className="card-text">Reach the first position in the ladder</p>
+                                                    </div>
+                                                </div>
+                                                
+
+                                                <div className="card">
+                                                    <div className="card-body">
+                                                        <h5 className="card-title">Bubu</h5>
+                                                        <p className="card-text">Reach the first position in the ladder</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
                                     </div>
-                                </div>
                             </div>
             	<Footer />
             </div>
