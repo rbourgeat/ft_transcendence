493b205879ba54e73e958551dfcfdff1675bebc4
macrespo42 <maxime.crespopro@gmail.com>
2022-03-17 20:44:52 +0100

    write render unit-test for not found page

--- frontend/typescript-react-app/src/components/NotFound/NotFound.tsx
+++ frontend/typescript-react-app/src/components/NotFound/NotFound.tsx
@@ -5,7 +5,6 @@ import Nav from '../Nav/Nav';
 /**
  * @malatini
  * Jolie page not found, a continuer
-<img src="https://www.pngkit.com/png/full/930-9306501_404-graphic-design.png"></img>
  */
 export default function NotFound() {
     return (
