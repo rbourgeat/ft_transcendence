bf68b4d125e74d923a3ffe5db71052e96b60a167
macrespo42 <maxime.crespopro@gmail.com>
2022-03-17 16:45:46 +0100

    page 404 basic design

--- frontend/typescript-react-app/src/components/NotFound/NotFound.tsx
+++ frontend/typescript-react-app/src/components/NotFound/NotFound.tsx
@@ -1,21 +1,23 @@
 import './NotFound.scss';
-// import Nav from '../Nav/Nav';
+import TypeAnimation from 'react-type-animation'
+import Nav from '../Nav/Nav';
 
 /**
  * @malatini
  * Jolie page not found, a continuer
+<img src="https://www.pngkit.com/png/full/930-9306501_404-graphic-design.png"></img>
  */
 export default function NotFound() {
     return (
         <div id="NotFound">
-            {/* <Nav /> */}
+            <Nav /> 
             <div className="container">
                 <div className="d-flex justify-content-center" id="oops--notfound">
-                    <h1><span id="oops">Oops !</span></h1>
-                    <h2><span id="page-not-found">Page not found</span></h2>
+					<h1><span id="oops">Oops !</span></h1>
+			    	<h2><span id="page-not-found">Page not found</span></h2>
                 </div>
-                <div className="d-flex justify-content-center" id="img--notfound">
-                    <img src="https://www.pngkit.com/png/full/930-9306501_404-graphic-design.png"></img>
+                <div className="d-flex justify-content-center" id="title--code">
+					<TypeAnimation className="title" cursor={false} sequence={["404!", 2500, '']} wrapper="h1" repeat={Infinity} />
                 </div>
             </div>
         </div>
