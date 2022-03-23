6bb7db9522a41c0f06bcda498d635208588f3dbd
malatini42 <malatini@student.42.fr>
2022-03-23 11:28:43 +0100

    petite modif texte blanc -> noir

--- frontend/typescript-react-app/src/components/Channels/CreateChan/CreateChan.tsx
+++ frontend/typescript-react-app/src/components/Channels/CreateChan/CreateChan.tsx
@@ -2,7 +2,7 @@ import './CreateChan.scss';
 import React, {Component} from "react";
 import Nav from "../../Nav/Nav";
 import {Modal, Button, Row, Col, Form} from "react-bootstrap";
-import MyVerticallyCenteredModal from "../../Utils/Modal/Modal";
+import CreateChanModal from "../../Utils/Modal/Modal";
 
 export default function CreateChan(props)
 {
@@ -14,10 +14,12 @@ export default function CreateChan(props)
             <div className="container">
                 <div className="row d-flex justify-content-center text-center">
                     <div className="col-7">
+					<p>You are not part of any channel.</p>
+					<p>Create a channel</p>
 					<Button variant="primary" onClick={() => setModalShow(true)}>
 						Launch vertically centered modal
 					</Button>
-					<MyVerticallyCenteredModal
+					<CreateChanModal
 						show={modalShow}
 						onHide={() => setModalShow(false)}
 					/>
