48600bc3767f0d2e1c6f3e7f0a8dd16cada3b637
malatini42 <malatini@student.42.fr>
2022-04-07 18:43:47 +0200

    push encore

--- frontend/typescript-react-app/src/components/People/All/All.tsx
+++ frontend/typescript-react-app/src/components/People/All/All.tsx
@@ -73,8 +73,8 @@ export default function All() {
 						<ul id="list--users--ul" className="list-group list-group-horizontal-lg">
 							{load == true ? console.log("Mapping") : ""}
 							{/*{mapUsers()}*/}
-							{/*{load == true ? users.map(users => <MiniDisplay key={users.login} login={users.login} status={users.status} avatar={users.avatar} />) : ""}*/}
-							{load == true ? <MiniDisplay key={users[0].login} login={users[0].login} status={users[0].status} avatar={users[0].avatar} /> : ""}
+							{load == true ? users.map(users => <MiniDisplay key={users.login} login={users.login} status={users.status} avatar={users.avatar} />) : ""}
+							{/*{load == true ? <MiniDisplay key={users[0].login} login={users[0].login} status={users[0].status} avatar={users[0].avatar} /> : ""}*/}
 							{/*{load == true ? <MiniDisplay key={users[1].login} login={users[1].login} status={users[1].status} avatar={users[1].avatar} /> : ""}*/}
 						</ul>
 					</div>
