79ff4ae860bab2b9f969bb945da785e4e483ddbd
malatini42 <malatini@student.42.fr>
2022-04-11 23:54:05 +0200

    particules debut ok

--- frontend/typescript-react-app/src/components/App/App.tsx
+++ frontend/typescript-react-app/src/components/App/App.tsx
@@ -9,6 +9,8 @@ import {
   Routes
 } from "react-router-dom";
 
+//import myParticles from "../Particules/Particules/";
+import Particles from "react-tsparticles";
 import Auth from '../Auth/Auth';
 import Welcome from '../Welcome/Welcome';
 import NotFound from '../NotFound/NotFound';
@@ -81,8 +83,111 @@ function App() {
     }
   }, []);
 
+  //let particlesInit = (main: any) => {
+  //  console.log(main);
+  //};
+
+  //let particlesLoaded = (container: any) => {
+  //  console.log(container);
+  //};
+
+  //let  particlesInit = (main) => {
+  //  console.log(main);
+
+  //  // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  //};
+
+  let particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <div id="main">
+      {/*<myParticules />*/}
+
+
+      <Particles
+      id="tsparticles"
+      //init={particlesInit}
+      //loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#00000",
+          },
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/welcome" element={<Welcome />} />
