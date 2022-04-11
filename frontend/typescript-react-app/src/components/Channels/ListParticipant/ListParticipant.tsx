077e15a8ca638548936693d22288f8de08e0594a
macrespo42 <macrespo@student.42.fr>
2022-04-11 15:19:08 +0200

    create channel button open a modal

--- frontend/typescript-react-app/src/components/Channels/ListParticipant/ListParticipant.tsx
+++ frontend/typescript-react-app/src/components/Channels/ListParticipant/ListParticipant.tsx
@@ -5,62 +5,50 @@ import Footer from "../../Footer/Footer";
 //import TimePicker from "react-time-picker";
 //import { useTimer } from 'react-timer-hook';
 import MyTimer from "./MyTimer/MyTimer";
+import {Button, Modal} from 'react-bootstrap';
 
 export interface ParticipantProps{
 }
 
-// export interface ParticipantState {
-// }
-
 export default function ListParticipant()
 {
-    // constructor(props: ParticipantProps)
-    // {
-    //     super(props);
+	const time = new Date();
+	time.setSeconds(time.getSeconds() + 600);
+	const [show, setShow] = React.useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
 
-    //     this.state = {}
-    // }
-
-    // componentDidMount() {}
-
-    // render()
-    // {
-
-        // const [value, onChange] = useState('10:00');
-        const time = new Date();
-        time.setSeconds(time.getSeconds() + 600);
-
-        return (
-            <div id="ListParticipant">
-                <h2 id="participant--title">Participants</h2>
-                <div id="sub--div" className="overflow-auto">
-                    <div id="participants--div">
-                            <Participant username="malatini" status="Online" owner={true} admin={true} />
-                            <Participant username="bahaas" status="Offline"/>
-                            <Participant username="rbourgea" status="Offline"/>
-                            <Participant username="darbib" status="Online"/>
-                            <Participant username="macrespo" status="Online" admin={true}/>
-                    </div>
-                    {/* <div className="row d-flex justify-content-center text-center">
-                        <button id="bann-button" className="btn btn-danger">Ban</button>
-                    </div>
-                    <div className="row d-flex justify-content-center text-center">
-                        <button id="mute-button" className="btn btn-warning">Mute</button>
-                    </div> */}
-                    <button id="bann-temp-button" className="btn btn-danger">Ban temporarily</button>
-                    <button id="mute-temp-button" className="btn btn-warning">Mute temporalily</button>
-                    {/*<MyTimer expiryTimestamp={time} />*/}
-                </div>
-
-                {/* <div id="roles">
-                    <h3 id="roles--title">Roles</h3>
-                    <p className="p--participants">Online <span className="online"></span></p>
-                    <p className="p--participants">Offline <span className="offline"></span></p>
-                    <p className="p--participants">Admin <span className="admin"></span></p>
-                    <p className="p--participants">Owner <span className="owner"></span></p>
-                </div> */}
-
-            <Footer />
-            </div>
-        );
-    }
+	return (
+		<div id="ListParticipant">
+			<h2 id="participant--title">Participants</h2>
+			<div id="sub--div" className="overflow-auto">
+				<div id="participants--div">
+					<Participant username="malatini" status="Online" owner={true} admin={true} />
+					<Participant username="bahaas" status="Offline"/>
+					<Participant username="rbourgea" status="Offline"/>
+					<Participant username="darbib" status="Online"/>
+					<Participant username="macrespo" status="Online" admin={true}/>
+				</div>
+					<button id="bann-temp-button" className="btn btn-danger">Ban temporarily</button>
+					<button id="mute-temp-button" className="btn btn-warning">Mute temporalily</button>
+					<br/>
+					<Button variant="secondary" onClick={handleShow}>Create channel</Button>
+					<Modal show={show} onHide={handleClose}>
+						<Modal.Header closeButton>
+							<Modal.Title>Modal heading</Modal.Title>
+						</Modal.Header>
+						<Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+						<Modal.Footer>
+							<Button variant="secondary" onClick={handleClose}>
+								Close
+							</Button>
+							<Button variant="primary" onClick={handleClose}>
+								Save Changes
+							</Button>
+						</Modal.Footer>
+					</Modal>
+				</div>
+				<Footer />
+			</div>
+	);
+}
