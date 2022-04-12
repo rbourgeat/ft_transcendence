3aa843507a91b87edc9862bc44b71f92cd517abc
bahaas <baudoin.haas@gmail.com>
2022-04-12 18:08:25 +0200

    FIX: button on people page to refresh

--- frontend/typescript-react-app/src/components/People/People.tsx
+++ frontend/typescript-react-app/src/components/People/People.tsx
@@ -21,7 +21,6 @@ export default function People(props: PeopleProps) {
 			{localStorage.getItem("loggedIn") != "true" ?
 				<>
 					<Nav />
-					<button onClick={update}>update</button>
 					<div className="container">
 						<div className="row d-flex justify-content-center text-center">
 							<div className="col-9">
@@ -35,6 +34,7 @@ export default function People(props: PeopleProps) {
 				:
 				<div id="people--div">
 					<Nav />
+					<button onClick={update}>update</button>
 					<div id="all">
 						<div className="row">
 							<br />
