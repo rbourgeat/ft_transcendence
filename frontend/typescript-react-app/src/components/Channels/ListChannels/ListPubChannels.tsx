a16bd494a616421f84b3b4e3a5bdcf291a9cc9c3
macrespo42 <macrespo@student.42.fr>
2022-04-12 17:01:04 +0200

    join public channel done

--- frontend/typescript-react-app/src/components/Channels/ListChannels/ListPubChannels.tsx
+++ frontend/typescript-react-app/src/components/Channels/ListChannels/ListPubChannels.tsx
@@ -18,13 +18,26 @@ export default function ListPubChannels() {
 				console.log(error);
 			})
 	}, [])
+
+	function joinChan(chanName) {
+		axios.post("http://localhost:3000/api/chat/join", {
+				"public": true,
+				"name": chanName 
+			})
+				.then(function (response) {
+					console.log(response);
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+	}
 	
 	return (
 		<div className="listChannels">
 			<h2>Public Channels</h2>
 			<ul id="list--channels--ul">
 				{pubChan.map(channel => 
-					channel.public === true ?	<li key={channel.id} className="channel--list">{channel.name}</li> : "" 
+					channel.public === true ?	<button key={channel.id} className="channel--list" onClick={() => {joinChan(channel.name)}}>{channel.name}</button> : "" 
 				)}
 			</ul>
 		</div>
