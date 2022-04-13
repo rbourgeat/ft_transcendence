aa7aa1e83363d1931c372a6289ab405b0cb6dd19
bahaas <baudoin.haas@gmail.com>
2022-04-13 20:16:11 +0200

    ADD: Progressbar

--- frontend/typescript-react-app/src/components/Dashboard/Badge/Badge.tsx
+++ frontend/typescript-react-app/src/components/Dashboard/Badge/Badge.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, useContext } from 'react';
 import './Badge.scss';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import axios from "axios";
+import { useParams } from 'react-router-dom'
 
-export default function Badge(props) {
+export interface ProfileProps {
+    login?: string,
+    total_wins?: number,
+    total_games?: number,
+    total_loss?: number,
+    rank?: number,
+    win_loss_ration?: string,
+    xp?: number,
+    points?: number,
+}
+
+
+export default function Badge(props: ProfileProps) {
+
     return (
         <div id="badge">
             <div className="container">
@@ -10,16 +26,16 @@ export default function Badge(props) {
                         <div className="col" id="badge--stats">
                             <br />
                             <h2 className="stats--h2">Statistics</h2>
-                            <br/>
+                            <br />
+                            <ProgressBar label={`level ${props.xp}%`} variant="success"></ProgressBar>
                             <p id="badge--rank" className="badge--stats-text">Rank: x</p>
                             <p id="badge--total-score" className="badge--stats-text">Total score : x</p>
                             <p id="badge--games" className="badge--stats-text">Games: x</p>
-                            <p id="badge--achievements" className="badge--stats-text">Achievements : x</p>
                             <br />
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
