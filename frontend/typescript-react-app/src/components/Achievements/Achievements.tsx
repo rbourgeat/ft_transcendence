a89a6a41796cc0950ded7b2ee17f8adfcec53046
bahaas <baudoin.haas@gmail.com>
2022-04-22 03:43:10 +0200

    WIP: reworked for animation checkmark

--- frontend/typescript-react-app/src/components/Achievements/Achievements.tsx
+++ frontend/typescript-react-app/src/components/Achievements/Achievements.tsx
@@ -38,10 +38,11 @@ export default function Achievement(props: AchievementsProps) {
             })
     }
 
+    /*
     function CheckMark() {
         return <svg className="checkmark"><BsCheckLg /></svg>;
     }
-
+*/
     function isUnlocked(name: string) {
         if (myArray.find(element => element == name))
             return true;
@@ -63,7 +64,7 @@ export default function Achievement(props: AchievementsProps) {
                 <div className="row" id="first-row">
                     <div className="card-group" id="group--one">
                         <div className={isUnlocked("BeAdmin") ? "card card--unlocked" : "card card--locked"}>
-                            {isUnlocked("BeAdmin") ? <CheckMark /> : ""}
+                            {isUnlocked("BeAdmin") ? <svg className="checkmark"><BsCheckLg /></svg> : ""}
                             <div className="card-body">
                                 <h5 className="card-title">Let's talk</h5>
                                 <p className="card-text">Admin of a channel</p>
@@ -71,7 +72,7 @@ export default function Achievement(props: AchievementsProps) {
                         </div>
 
                         <div className={isUnlocked("5Row") ? "card card--unlocked" : "card card--locked"}>
-                            {isUnlocked("5Row") ? <CheckMark /> : ""}
+                            {isUnlocked("5Row") ? <svg className="checkmark"><BsCheckLg /></svg> : ""}
                             <div className="card-body">
                                 <h5 className="card-title">On fire</h5>
                                 <p className="card-text">Win 5 games</p>
@@ -81,7 +82,7 @@ export default function Achievement(props: AchievementsProps) {
                         </div>
 
                         <div className={isUnlocked("1000Game") ? "card card--unlocked" : "card card--locked"}>
-                            {isUnlocked("1000Game") ? <CheckMark /> : ""}
+                            {isUnlocked("1000Game") ? <svg className="checkmark"><BsCheckLg /></svg> : ""}
                             <div className="card-body">
                                 <h5 className="card-title">True g4m3r</h5>
                                 <p className="card-text">Play 1000 games</p>
@@ -95,7 +96,7 @@ export default function Achievement(props: AchievementsProps) {
                 <div className="row" id="second-row">
                     <div className="card-group">
                         <div className={isUnlocked("AddFriend") ? "card card--unlocked" : "card card--locked"}>
-                            {isUnlocked("AddFriend") ? <CheckMark /> : ""}
+                            {isUnlocked("AddFriend") ? <svg className="checkmark"><BsCheckLg /></svg> : ""}
                             <div className="card-body">
                                 <h5 className="card-title">How I met</h5>
                                 <p className="card-text">Have one friend</p>
@@ -104,7 +105,7 @@ export default function Achievement(props: AchievementsProps) {
                         </div>
 
                         <div className={isUnlocked("FirstGame") ? "card card--unlocked" : "card card--locked"}>
-                            {isUnlocked("FirstGame") ? <CheckMark /> : ""}
+                            {isUnlocked("FirstGame") ? <svg className="checkmark"><BsCheckLg /></svg> : ""}
                             <div className="card-body">
                                 <h5 className="card-title">The Pong</h5>
                                 <p className="card-text">Play your first game</p>
@@ -114,7 +115,7 @@ export default function Achievement(props: AchievementsProps) {
                         </div>
 
                         <div className={isUnlocked("42") ? "card card--unlocked" : "card card--locked"}>
-                            {isUnlocked("42") ? <CheckMark /> : ""}
+                            {isUnlocked("42") ? <svg className="checkmark"><BsCheckLg /></svg> : ""}
                             <div className="card-body">
                                 <h5 className="card-title">Mistery</h5>
                                 <p className="card-text">????????????????</p>
