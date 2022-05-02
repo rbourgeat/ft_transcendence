c1359efef7f57d6494d36745fbfa171bb370f716
malatini42 <malatini@student.42.fr>
2022-05-02 10:20:24 +0200

    mini update images

---
+++ frontend/typescript-react-app/webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    "postcss-preset-env",
+                    {
+                      // Options
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
