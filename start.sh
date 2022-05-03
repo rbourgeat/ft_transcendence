51c10988665625e7c55b3c4a931dcb2dd1e4b874
malatini42 <malatini@student.42.fr>
2022-05-03 15:02:06 +0200

    start.sh petite modif mac bis

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
 #Check en fonction de si c'est un mac ou un linux
 if [[ "$OSTYPE" == "darwin"* ]]; then
 	echo "TEST=$(ipconfig getifaddr en0 | cut -d ' ' -f 1)" >> .env
+	export TEST=$(ipconfig getifaddr en0 | cut -d ' ' -f 1)
+	echo "REACT_APP_IP=$(ipconfig getifaddr en0 | cut -d ' ' -f 1)" >> .env
+	export REACT_APP_IP=$(ipconfig getifaddr en0 | cut -d ' ' -f 1)
 else 
 	echo "TEST=$(hostname -I | cut -d ' ' -f 1)" >> .env
+	export TEST=$(hostname -I | cut -d ' ' -f 1)
+	echo "REACT_APP_IP=$(hostname -I | cut -d ' ' -f 1)" >> .env
+	export REACT_APP_IP=$(hostname -I | cut -d ' ' -f 1)
 fi
-export TEST=$(hostname -I | cut -d ' ' -f 1)
-echo "REACT_APP_IP=$(hostname -I | cut -d ' ' -f 1)" >> .env
-export REACT_APP_IP=$(hostname -I | cut -d ' ' -f 1)
 
+#echo "REACT_APP_IP=$(hostname -I | cut -d ' ' -f 1)" >> .env
+#export REACT_APP_IP=$(hostname -I | cut -d ' ' -f 1)
 #echo "REACT_APP_IP=localhost" >> .env
 #echo "TEST=localhost" >> .env
 #export REACT_APP_IP=localhost
