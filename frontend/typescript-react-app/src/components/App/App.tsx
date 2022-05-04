7807064ab294ff6e3413fca45d141dd42c507a7d
malatini42 <malatini@student.42.fr>
2022-05-04 11:53:16 +0200

    modif verif connexion encore

--- frontend/typescript-react-app/src/components/App/App.tsx
+++ frontend/typescript-react-app/src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'
 import './App.scss';
 import {
@@ -7,7 +7,6 @@ import {
   Route,
   Routes
 } from "react-router-dom";
-
 import Particles from "react-tsparticles";
 import Auth from '../Auth/Auth';
 import Welcome from '../Welcome/Welcome';
@@ -47,21 +46,8 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
   const calledOnce = React.useRef(false);
 
-  const value = useMemo(() =>
-    ({ user, setUser }), [user, setUser]);
-
-
-
-	/*async*/ function getUser() {
-
-    //if (localStorage.getItem("loggedIn") == "false")
-    //{
-    //  return ;
-    //}
-
-    //console.log(env.REACT_APP_IP)
+	function getUser() {
     let url = url_begin.concat(":3000/api/auth/");
-    console.log("url 2 is " + url);
     let username = "";
     axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
     axios.defaults.withCredentials = true;
@@ -71,61 +57,13 @@ function App() {
         setUsername(username);
       })
       .catch((err) => {
-        //console.log("Error while getting api auth");
         ;
       })
   }
 
-  /*
-  const [socket, setSocket] = React.useState(io("http://".concat(process.env.REACT_APP_IP).concat(":3000/chat"), { query: { username: username } }));
-*/
   useEffect(() => {
     getUser();
-    //  setSocket(io("http://".concat(process.env.REACT_APP_IP).concat(":3000/chat"), { query: { username: username } }));
   }, []);
-
-  //   function acceptInvitePlay()
-  //     {
-  //         window.top.location = url_begin.concat(":3030/game?vs=").concat(selectedUser);;
-  //     }
-
-  //   const InvitetoPlay = () => {
-  //     return(
-  //     <div>
-  //         Somebody wants to play with you !
-  //         <button className="btn btn-dark" onClick={acceptInvitePlay}>Accept</button>
-  //     </div>)
-  // }
-
-  //   var socket2 = io(url_begin.concat(":3000/game"), { query: { username: username } });
-
-  //   socket2.on('inviteToPlay', (...args) => {
-  //     console.log("step 1")
-
-  //   socket2.on('inviteToPlay', (...args) => {
-  //     console.log("step 1")
-
-  //     if (username == args[0])
-  //       selectedUser = args[1];
-  //     else if (username == args[1])
-  //       selectedUser = args[0];
-  //     else
-  //       return;
-  //       console.log("step 2")
-
-  //     toast.dark(<InvitetoPlay />, {
-  //               position: "top-right",
-  //               autoClose: 10000,
-  //               hideProgressBar: false,
-  //               closeOnClick: false,
-  //               pauseOnHover: false,
-  //               draggable: false,
-  //               closeButton: false
-  //               //progress: undefined
-  //           });
-  //   });
-
-
 
   useEffect(() => {
     if (calledOnce.current) {
@@ -142,98 +80,18 @@ function App() {
       localStorage.setItem("2faverif", "false");
   }, []);
 
-  let particlesLoaded = (container) => {
-    //console.log(container);
-    ;
-  };
-
   return (
     <div id="main">
-      {/* <Particles
-				id="tsparticles"
-				options={{
-					background: {
-						color: {
-							value: "#00000",
-						},
-					},
-					fpsLimit: 120,
-					interactivity: {
-						events: {
-							onClick: {
-								enable: false,
-								mode: "push",
-							},
-							onHover: {
-								enable: false,
-								mode: "repulse",
-							},
-							resize: false,
-						},
-						modes: {
-							bubble: {
-								distance: 100,
-								duration: 5,
-								opacity: 0.8,
-								size: 20,
-							},
-							push: {
-								quantity: 20,
-							},
-							repulse: {
-								distance: 200,
-								duration: 0.4,
-							},
-						},
-					},
-					particles: {
-						color: {
-							value: "#54CFE8"
-						},
-						links: {
-							color: "#ffffff",
-							distance: 150,
-							enable: false,
-							opacity: 0.5,
-							width: 1,
-						},
-						collisions: {
-							enable: true,
-						},
-						move: {
-							direction: "bottom",
-							enable: false,
-							outMode: "bounce",
-							random: false,
-							speed: 2,
-							straight: false,
-						},
-						number: {
-							density: {
-								enable: true,
-								area: 120,
-							},
-							value: 10,
-						},
-						opacity: {
-							value: 0.3,
-							random: true,
-						},
-						shape: {
-							type: "circle",
-						},
-						size: {
-							random: false,
-							value: 40,
-						},
-					},
-					detectRetina: false
-				}}
-			/> */}
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/welcome" element={<Welcome />} />
         <Route path="/auth" element={<Auth />} />
+        {localStorage.getItem("loggedIn") == "false" ? 
+        <>
+          <Route path="*" element={<Auth />} />
+        </> 
+        : ""
+        }
         {(localStorage.getItem("loggedIn") == "true" &&
           ((localStorage.getItem("2fa") == "true" && localStorage.getItem("2faverif") == "true") || (localStorage.getItem("2fa") == "false" && localStorage.getItem("2faverif") == "false")))
           ?
@@ -254,9 +112,9 @@ function App() {
 
         {localStorage.getItem("loggedIn") == "true" && localStorage.getItem("2fa") == "true" && localStorage.getItem("2faverif") == "false" ?
           <Route path="*" element={<Login2fa />} /> : ""}
-        {(localStorage.getItem("2fa") == "true" && localStorage.getItem("2faverif") == "false") ?
+        {/*{(localStorage.getItem("2fa") == "true" && localStorage.getItem("2faverif") == "false") ?
           <Route path="*" element={<Login2FA />} />
-          : ""}
+          : ""}*/}
       </Routes>
       <ToastContainer
         position="top-right"
